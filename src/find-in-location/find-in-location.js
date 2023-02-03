c3244f8ed5fc68605ce1b0234e8325b46613a3e9
Emilis Mitrikas <emilis.mitrikas@gmail.com>
2023-02-03 12:09:24 +0000

    Changed "type" to "commonjs". As otherwise, this is not compatible with flow elements

--- src/find-in-location/find-in-location.js
+++ src/find-in-location/find-in-location.js
@@ -1,22 +1,28 @@
-import fs from "fs";
-import path from "path";
-export const notExistingOptions = { returnEmptyResults: "returnEmptyResults", throwError: "throwError" };
-export const searchTarget = { both: "both", files: "files", folders: "folders" };
-export const searchEngineOptionsDefaults = {
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SearchEngine = exports.searchEngineOptionsDefaults = exports.searchTarget = exports.notExistingOptions = void 0;
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+exports.notExistingOptions = { returnEmptyResults: "returnEmptyResults", throwError: "throwError" };
+exports.searchTarget = { both: "both", files: "files", folders: "folders" };
+exports.searchEngineOptionsDefaults = {
     allowedExt: [],
     allowPartialMatch: true,
     caseSensitiveMatch: false,
     returnTypes: { full: true, name: true, nameProper: true },
     scanDepth: 0,
-    searchTarget: searchTarget.both,
-    ifHaystackDoesNotExist: notExistingOptions.throwError,
-    ifNeedleDoesNotExist: notExistingOptions.returnEmptyResults
+    searchTarget: exports.searchTarget.both,
+    ifHaystackDoesNotExist: exports.notExistingOptions.throwError,
+    ifNeedleDoesNotExist: exports.notExistingOptions.returnEmptyResults
 };
 //Adds default values to the options and does some processing on them
 function makeSenseOutOfOptions(opt) {
     const options = opt || {};
     if (options.allowedExt === undefined) {
-        options.allowedExt = searchEngineOptionsDefaults.allowedExt;
+        options.allowedExt = exports.searchEngineOptionsDefaults.allowedExt;
     }
     else {
         for (let i = 0; i < options.allowedExt.length; i++) {
@@ -28,31 +34,31 @@ function makeSenseOutOfOptions(opt) {
         }
     }
     if (options.allowPartialMatch === undefined) {
-        options.allowPartialMatch = searchEngineOptionsDefaults.allowPartialMatch;
+        options.allowPartialMatch = exports.searchEngineOptionsDefaults.allowPartialMatch;
     }
     if (options.caseSensitiveMatch === undefined) {
-        options.caseSensitiveMatch = searchEngineOptionsDefaults.caseSensitiveMatch;
+        options.caseSensitiveMatch = exports.searchEngineOptionsDefaults.caseSensitiveMatch;
     }
     if (options.returnTypes === undefined) {
-        options.returnTypes = searchEngineOptionsDefaults.returnTypes;
+        options.returnTypes = exports.searchEngineOptionsDefaults.returnTypes;
     }
     if (options.scanDepth === undefined || +options.scanDepth < 0) {
-        options.scanDepth = searchEngineOptionsDefaults.scanDepth;
+        options.scanDepth = exports.searchEngineOptionsDefaults.scanDepth;
     }
     if (options.searchTarget === undefined) {
-        options.searchTarget = searchEngineOptionsDefaults.searchTarget;
+        options.searchTarget = exports.searchEngineOptionsDefaults.searchTarget;
     }
     if (options.ifHaystackDoesNotExist === undefined) {
-        options.ifHaystackDoesNotExist = searchEngineOptionsDefaults.ifHaystackDoesNotExist;
+        options.ifHaystackDoesNotExist = exports.searchEngineOptionsDefaults.ifHaystackDoesNotExist;
     }
     if (options.ifNeedleDoesNotExist === undefined) {
-        options.ifNeedleDoesNotExist = searchEngineOptionsDefaults.ifNeedleDoesNotExist;
+        options.ifNeedleDoesNotExist = exports.searchEngineOptionsDefaults.ifNeedleDoesNotExist;
     }
     return options;
 }
 //Returns a new "searchResult" object
 function initiateSearchResults(returnTypes) {
-    returnTypes = returnTypes || searchEngineOptionsDefaults.returnTypes;
+    returnTypes = returnTypes || exports.searchEngineOptionsDefaults.returnTypes;
     const result = {
         results: {},
         stats: {
@@ -75,7 +81,7 @@ function initiateSearchResults(returnTypes) {
     }
     return result;
 }
-export class SearchEngine {
+class SearchEngine {
     options;
     makeSenseOutOfOptions(options) {
         return makeSenseOutOfOptions(options);
@@ -86,11 +92,11 @@ export class SearchEngine {
     searchRecursively(results, needle, haystack, depth) {
         let newDepth = depth - 1;
         results.stats.foldersScanned++;
-        for (let dirent of fs.readdirSync(haystack, { withFileTypes: true, encoding: "utf-8" })) {
+        for (let dirent of fs_1.default.readdirSync(haystack, { withFileTypes: true, encoding: "utf-8" })) {
             results.stats.entitiesCompared++;
             const hayOriginal = dirent.name;
             const hay = this.options.caseSensitiveMatch ? hayOriginal : hayOriginal.toLowerCase();
-            const fullPath = path.join(haystack, hayOriginal).replaceAll("\\", "/");
+            const fullPath = path_1.default.join(haystack, hayOriginal).replaceAll("\\", "/");
             const isDir = dirent.isDirectory();
             if (isDir && newDepth >= 0) {
                 this.searchRecursively(results, needle, fullPath, newDepth);
@@ -101,15 +107,15 @@ export class SearchEngine {
             else if (!this.options.allowPartialMatch && hay !== needle) {
                 continue;
             }
-            const parsedName = path.parse(hayOriginal);
+            const parsedName = path_1.default.parse(hayOriginal);
             if (!isDir && this.options.allowedExt && this.options.allowedExt.length && !this.options.allowedExt.includes(parsedName.ext.toLowerCase())) {
                 continue;
             }
-            if (this.options.searchTarget !== searchTarget.both) {
-                if (this.options.searchTarget === searchTarget.folders && dirent.isFile()) {
+            if (this.options.searchTarget !== exports.searchTarget.both) {
+                if (this.options.searchTarget === exports.searchTarget.folders && dirent.isFile()) {
                     continue;
                 }
-                if (this.options.searchTarget === searchTarget.files && isDir) {
+                if (this.options.searchTarget === exports.searchTarget.files && isDir) {
                     continue;
                 }
             }
@@ -129,22 +135,22 @@ export class SearchEngine {
         needle = this.options.caseSensitiveMatch ? needle : needle.toLowerCase();
         const result = this.initiateSearchResults();
         //Checking if haystack exist and acting based on what's in the options
-        if (!fs.existsSync(haystack)) {
-            if (this.options.ifHaystackDoesNotExist === notExistingOptions.throwError) {
+        if (!fs_1.default.existsSync(haystack)) {
+            if (this.options.ifHaystackDoesNotExist === exports.notExistingOptions.throwError) {
                 throw new Error(`Haystack "${haystack}" does not exist!`);
             }
-            else if (this.options.ifHaystackDoesNotExist === notExistingOptions.returnEmptyResults) {
+            else if (this.options.ifHaystackDoesNotExist === exports.notExistingOptions.returnEmptyResults) {
                 return result;
             }
             else {
-                throw new Error(`Option ifHaystackDoesNotExist has invalid value "${this.options.ifHaystackDoesNotExist}", allowed values are: "${Object.values(notExistingOptions).join(`", "`)}"!`);
+                throw new Error(`Option ifHaystackDoesNotExist has invalid value "${this.options.ifHaystackDoesNotExist}", allowed values are: "${Object.values(exports.notExistingOptions).join(`", "`)}"!`);
             }
         }
         result.stats.timeTaken = Date.now();
-        this.searchRecursively(result, needle, haystack, this.options.scanDepth || searchEngineOptionsDefaults.scanDepth || 0);
+        this.searchRecursively(result, needle, haystack, this.options.scanDepth || exports.searchEngineOptionsDefaults.scanDepth || 0);
         result.stats.timeTaken = Date.now() - result.stats.timeTaken;
         //Checking if needle exist and acting based on what's in the options
-        if (result.stats.resultsFound < 1 && this.options.ifNeedleDoesNotExist === notExistingOptions.throwError) {
+        if (result.stats.resultsFound < 1 && this.options.ifNeedleDoesNotExist === exports.notExistingOptions.throwError) {
             throw new Error(`No results were found for needle "${needle}" in haystack "${haystack}"!`);
         }
         return result;
@@ -153,3 +159,4 @@ export class SearchEngine {
         this.options = this.makeSenseOutOfOptions(options);
     }
 }
+exports.SearchEngine = SearchEngine;
